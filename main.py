efd939bb7ef396f0885b9bdad3bda3ad8661321a
ulysse <juget.ulysse@gmail.com>
2022-12-05 14:37:49 -0500

    added support for cross files

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import parser #parser.py
+import memory #memory.py
 
 #Don't start the program before main is called
 if __name__ == '__main__':
-    
+    full_code = parser.import_file("asm_samples/example1.txt")
+    full_code = parser.unclutter(full_code)
+
+    data, code = parser.split_data_code(full_code)
+
+    print(data)
+    print(code)
